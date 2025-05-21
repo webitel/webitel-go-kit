6d8a915527f8d3211d7917764e8b895c615a1890
vhunkalo <graperz123@gmail.com>
2025-05-21 16:28:05 +0300

    WTEL-6939 naming refactoring

--- infra/pubsub/rabbitmq/pkg/adapter/wlog/wlog.go
+++ infra/pubsub/rabbitmq/pkg/adapter/wlog/wlog.go
@@ -5,23 +5,23 @@ import (
 	"github.com/webitel/wlog"
 )
 
-type AdapterLogger struct {
+type Logger struct {
 	base *wlog.Logger
 }
 
-func NewAdapterLogger(base *wlog.Logger) *AdapterLogger {
-	return &AdapterLogger{base: base}
+func NewWlogLogger(base *wlog.Logger) *Logger {
+	return &Logger{base: base}
 }
 
-func (l *AdapterLogger) Info(msg string, args ...any) {
+func (l *Logger) Info(msg string, args ...any) {
 	l.base.Info(fmt.Sprintf(msg, args...))
 }
 
-func (l *AdapterLogger) Warn(msg string, args ...any) {
+func (l *Logger) Warn(msg string, args ...any) {
 	l.base.Warn(fmt.Sprintf(msg, args...))
 }
 
-func (l *AdapterLogger) Error(msg string, err error, args ...any) {
+func (l *Logger) Error(msg string, err error, args ...any) {
 	formattedMsg := fmt.Sprintf(msg, args...)
 	l.base.Error(fmt.Sprintf("%s: %v", formattedMsg, err))
 }
